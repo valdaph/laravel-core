b0cde3a2356c1508fd9789cfa4d9c5c6f824b8e7
John Ryan Camatog <john.ryan.camatog@gmail.com>
2018-01-21 15:59:22 +0800

    Add missing resource functions to HandlesModelCrud trait

--- app/Traits/HandlesModelCrud.php
+++ app/Traits/HandlesModelCrud.php
@@ -10,13 +10,15 @@ use ScoutElastic\Searchable as ElasticSearchable;
 
 trait HandlesModelCrud
 {
+    use TransformsResponses;
+
     /**
      * The resource to be used.
      *
      * @var \Illuminate\Database\Eloquent\Model
      */
     protected $model;
-    
+
     /**
      * The columns exluded from queries.
      *
@@ -30,7 +32,118 @@ trait HandlesModelCrud
     ];
 
     /**
-     * Get the appropriate builder for the querying the resource.
+     * Display a listing of the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        return $this->okResponse($this->queryModel($request));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate($this->createRules());
+
+        $createdModel = $this->model->create($request->all());
+
+        return $this->createdResponse($createdModel);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  integer  $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Request $request, $id)
+    {
+        return $this->okResponse(
+            $this->loadRelations($request, $this->model->findOrFail($id))
+        );
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  integer  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate($this->updateRules());
+
+        $isUpdated = $this->model->findOrFail($id)->update($request->all());
+
+        return $this->noContentResponse($isUpdated);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  integer  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Request $request, $id)
+    {
+        $isDeleted = $this->model->findOrFail($id)->delete();
+
+        return $this->noContentResponse($isDeleted);
+    }
+
+    /**
+     * Get the rules for storing a created resource.
+     *
+     * @return array
+     */
+    protected function createRules()
+    {
+        return [];
+    }
+
+    /**
+     * Get the rules for updating a specified resource.
+     *
+     * @return array
+     */
+    protected function updateRules()
+    {
+        return [];
+    }
+
+    /**
+     * Query resource based on request parameters.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @return array
+     */
+    protected function queryModel(Request $request, $model = null)
+    {
+        $model = $model ? : $this->model;
+
+        $results = $this->getBuilder($request, $model)
+            ->applyScopes($request, $model)
+            ->applyBasicQueries($request, $model)
+            ->applyCustomQueries($request, $model)
+            ->transformResults($request, $model);
+
+        return $this->loadRelations($request, $results);
+    }
+
+    /**
+     * Get the appropriate builder for querying the resource.
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  mixed  $model
@@ -66,6 +179,45 @@ trait HandlesModelCrud
     }
 
     /**
+     * Apply initial queries to model.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  mixed  $model
+     * @return $this
+     */
+    protected function applyScopes(Request $request, &$model)
+    {
+        if (!$request->has('scope')) {
+            return $this;
+        }
+
+        $scopes = explode(',', $request->query('scope'));
+
+        foreach ($scopes as $scope) {
+            try {
+                $model = $model->{camel_case($scope)}();
+            } catch (\Exception $e) {
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * Apply basic queries to model.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  mixed  $model
+     * @return array
+     */
+    protected function applyBasicQueries(Request $request, &$model)
+    {
+        return $this->applyWhere($request, $model)
+            ->applyNot($request, $model)
+            ->applySort($request, $model);
+    }
+
+    /**
      * Apply where conditions to model.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -76,7 +228,7 @@ trait HandlesModelCrud
     protected function applyWhere(Request $request, &$model)
     {
         $where = array();
-        
+
         $tableName = $this->model->getTable();
         $strict = filter_var($request->query('strict', false), FILTER_VALIDATE_BOOLEAN);
 
@@ -114,6 +266,133 @@ trait HandlesModelCrud
     }
 
     /**
+     * Apply not conditions to model.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  mixed  $model
+     * @return array
+     */
+    protected function applyNot(Request $request, &$model)
+    {
+        if (!$request->has('not')) {
+            return $this;
+        }
+
+        $notParameters = explode('|', $request->query('not'));
+
+        foreach ($notParameters as $n) {
+            $not = explode(':', $n);
+
+            if (count($not) == 1) {
+                $model->whereNotIn('id', explode(',', $not[0]));
+            } elseif (count($not) == 2 && Schema::hasColumn($this->model->getTable(), $not[0])) {
+                $model->whereNotIn($not[0], explode(',', $not[1]));
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * Apply sorting conditions to model.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  mixed  $model
+     * @return $this
+     */
+    protected function applySort(Request $request, &$model)
+    {
+        if (!$request->has('sort')) {
+            return $this;
+        }
+
+        $sortParameters = explode(',', $request->query('sort'));
+
+        foreach ($sortParameters as $sp) {
+            $sort = explode(':', $sp);
+
+            if (Schema::hasColumn($this->model->getTable(), $sort[0])) {
+                $model->orderBy($sort[0], count($sort) == 2 ? $sort[1] : 'asc');
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * Apply custom queries to model.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  mixed  $model
+     * @return $this
+     */
+    protected function applyCustomQueries(Request $request, &$model)
+    {
+        return $this;
+    }
+
+    /**
+     * Transform results to based on query.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  mixed  $model
+     * @return array
+     */
+    protected function transformResults(Request $request, $model)
+    {
+        $page = $request->query('page');
+        $limit = $request->query('limit');
+        $take = $request->query('take');
+        $return = $request->query('return');
+
+        switch ($return) {
+            case 'count':
+                return $model->get()->count();
+                break;
+
+            case 'first':
+                return $model->first();
+                break;
+
+            default:
+                if (is_numeric($page) && is_numeric($limit)) {
+                    return $model->paginate($limit);
+                }
+
+                if (is_numeric($take)) {
+                    return $model->take($take)->get();
+                }
+
+                return $model->get();
+        }
+    }
+
+    /**
+     * Load relations of results.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  mixed  $results
+     * @return array
+     */
+    protected function loadRelations(Request $request, $results)
+    {
+        if (!$request->has('with')) {
+            return $results;
+        }
+
+        $relations = explode(',', $request->query('with'));
+
+        foreach ($relations as $relation) {
+            try {
+                $results->load(camel_case($relation));
+            } catch (\Exception $e) {
+            }
+        }
+
+        return $results;
+    }
+
+    /**
      * Check if the request is searching for a resource.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -125,7 +404,7 @@ trait HandlesModelCrud
 
         $isSearchable = in_array(Searchable::class, $modelTraits)
             || in_array(ElasticSearchable::class, $modelTraits);
-        
+
         return $request->filled('q') && $isSearchable;
     }
 
