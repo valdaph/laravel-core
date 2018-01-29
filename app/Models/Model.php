aa9f2c4f6f78aadaa740550e1073586b1e945034
John Ryan Camatog <john.ryan.camatog@gmail.com>
2018-01-29 22:02:35 +0800

    Remove cascading soft delete from base model

--- app/Models/Model.php
+++ app/Models/Model.php
@@ -2,11 +2,10 @@
 
 namespace Valda\Models;
 
-use Iatstuti\Database\Support\CascadeSoftDeletes;
 use Illuminate\Database\Eloquent\Model as BaseModel;
 use Valda\Traits\SilencesModelEvents;
 
 class Model extends BaseModel
 {
-    use CascadeSoftDeletes, SilencesModelEvents;
+    use SilencesModelEvents;
 }
