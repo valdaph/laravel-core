25173d1c319aa1f29cf294a362cf3d225f6ff23a
John Ryan Camatog <john.ryan.camatog@gmail.com>
2018-05-04 15:31:46 +0800

    Allow constants snippet to accept a custom constants config

--- views/constants.blade.php
+++ views/constants.blade.php
@@ -30,3 +30,36 @@
         })(window);
     </script>
 @endif
+
+@if (isset($constants) && config($constants))
+    <script>
+        (function(global) {
+            let {{ studly_case($constants) }} = {};
+
+            @foreach (config($constants) as $key => $value)
+                @if (is_array($value) && count($value) > 0)
+                    let {{ $key }} = {};
+
+                    @foreach ($value as $k => $v)
+                        {{ $key }}.{{ $k }} = '{{ $v }}';
+                    @endforeach
+
+                    Object.freeze({{ $key }});
+
+                    {{ studly_case($constants) }}.{{ $key }} = {{ $key }};
+                @elseif (is_string($value))
+                    {{ studly_case($constants) }}.{{ $key }} = '{{ $value }}';
+                @endif
+            @endforeach
+
+            Object.freeze({{ studly_case($constants) }});
+
+            Object.defineProperty(global, '{{ studly_case($constants) }}', {
+                value: {{ studly_case($constants) }},
+                writable: false,
+                enumerable: true,
+                configurable: true,
+            });
+        })(window);
+    </script>
+@endif
